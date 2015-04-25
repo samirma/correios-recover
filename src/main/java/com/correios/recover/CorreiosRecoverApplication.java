b8c202c06ef254f8cdbd3d9b0bf7488424e65c26
samirma <samir.ma@gmail.com>
2015-04-25 15:45:49 -0300

    spring boot added

---
+++ src/main/java/com/correios/recover/CorreiosRecoverApplication.java
@@ -0,0 +1,12 @@
+package com.correios.recover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreiosRecoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorreiosRecoverApplication.class, args);
+    }
+}
